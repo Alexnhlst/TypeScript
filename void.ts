482dfb10a897423ee0c9d447c2f235b5f4575214
Alexnhlst <agostinacchioalessandro@gmail.com>
2022-07-02 23:31:14 +0200

    Created void section

---
+++ void.ts
@@ -0,0 +1,5 @@
+// void represents the lack of any type
+// the only recommended use for void is as the return type for functions that don't return any value
+const log = (msg: string): void => {
+  console.log(msg);
+};
