c11ee96c3822916d103ab6ca7ffcdeb595c9c8ed
Alexnhlst <agostinacchioalessandro@gmail.com>
2022-07-02 19:47:25 +0200

    Created an enum

---
+++ FinanceMe/enums/Category.ts
@@ -0,0 +1,13 @@
+// an enum holds a set of named members
+// it can be considered as a collection or grouping of costants
+// the enum's name can be used as a type in locations where any of its members' usage is expected
+enum Category {
+  Food,
+  Entertainment,
+  Housing,
+  Utilities,
+  Transportation,
+  Misc,
+}
+
+export default Category;
