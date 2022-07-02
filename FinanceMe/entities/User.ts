3a2b994a2b2004e52ae057860cb4cc736d897e29
Alexnhlst <agostinacchioalessandro@gmail.com>
2022-07-02 17:35:30 +0200

    Created a user class with private properties

--- FinanceMe/entities/User.ts
+++ FinanceMe/entities/User.ts
@@ -1,28 +1,55 @@
 // A class is an OOP concept that allow the instatiation of objects
 // in TS, the name of the class is the name of the default constructor,
 // as well as the name of the type of the objects created by the constructor
-class User {
-  // Public properties with a declared type
-  name: string;
-  age: number;
-  constructor(name: string, age: number) {
-    this.name = name;
-    this.age = age;
-  }
-  // Public method
-  printUserInfo() {
-    console.log(`name: ${this.name}, age: ${this.age}`);
-  }
-}
-
+// class User {
+//   // Public properties with a declared type
+//   name: string;
+//   age: number;
+//   constructor(name: string, age: number) {
+//     this.name = name;
+//     this.age = age;
+//   }
+//   // Public method
+//   printUserInfo() {
+//     console.log(`name: ${this.name}, age: ${this.age}`);
+//   }
+// }
 // instance of the class - mike is type User
-let mike: User;
-
+// let mike: User;
 /* error: constructor needs 2 parameters
 mike = new User();
 error: first parameter must be type string
 mike = new User(1, 2);
 */
+// mike = new User("Michael", 32);
+// mike.printUserInfo();
 
-mike = new User("Michael", 32);
-mike.printUserInfo();
+class User {
+  // Private class properties
+  private name: string;
+  private age: number;
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+  // Getters - methods to retrieve properties
+  // Setters - not yet implemented, methods to update properties
+  getName() {
+    return this.name;
+  }
+  getAge() {
+    return this.age;
+  }
+  printUserInfo() {
+    console.log(`name: ${this.name}, age: ${this.age}`);
+  }
+}
+
+const katie = new User("Katherine", 35);
+/* error: these properties are private
+katie.name;
+katie.getAge;
+*/
+
+console.log(katie.getName());
+console.log(katie.getAge());
