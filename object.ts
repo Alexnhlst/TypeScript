b86ec289d6f4069d1de273e74f7e17e5a906ec04
Alexnhlst <agostinacchioalessandro@gmail.com>
2022-07-03 11:31:06 +0200

    Created object properties section

--- object.ts
+++ object.ts
@@ -23,3 +23,43 @@ console.log(numProducts);
 // {} refers to the empty object type, which in practice behaves the same as Object
 // they are misleading as they are assignable to primitives in addition to objects
 // object requires a type that is only assignable to non-primitives
+
+// structural typing (or duck typing) means that the structure of an object's type determines its assignability
+// in other languages, such as C++ or Java, nominal typing uses the name of an object's type to determine its assignability
+// the duck test states "If it walks like a duck and it quacks like a duck, then it must be a duck"
+type Phone = {
+  name: string;
+  id: string;
+  rating: number;
+};
+
+let iPhone: Phone;
+iPhone = {
+  name: "iPhone",
+  id: "uniqueID123",
+  rating: 10,
+};
+// the shape of the literal has to exactly match the shape that is defined in the Phone type
+/* the following example will produce an error since the name field is missing
+and two excess fields are present
+iPhone = {
+  title: "iPhone",
+  id: "uniqueID123",
+  rating: 3,
+  // excess fields
+  randomField: "Hi mom!", 
+  anotherRandomField: 2, 
+};
+*/
+const android = {
+  title: "Android",
+  name: "Android",
+  id: "uniqueID123",
+  rating: 3,
+  randomField: "Hi dad!",
+  anotherRandomField: 10000,
+};
+// when assigning it to an existing object, only the missing field will produce an error
+iPhone = android;
+// object literals undergo what is known as excess property checking
+// it ensures that only properties defined in the object type are present
