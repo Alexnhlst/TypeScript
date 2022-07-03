fbd634f58204384c3975ff47f919e50d1dc28db2
Alexnhlst <agostinacchioalessandro@gmail.com>
2022-07-03 13:49:01 +0200

    Created default parameter section

---
+++ functions.ts
@@ -0,0 +1,33 @@
+// functions are the backbone of code reusability
+// TS makes it painless to add type annotations to functions
+// typing functions consist of typing the parameters and typing the return value
+// the line that defines the input types and return type is known as the function signature
+const toNumber = (str: string | number): number => {
+  return Number(str);
+};
+// is common sense for some of a function's parameters to be optional
+// optional parameters must come after any and all required parameteres
+const formatMoney = (
+  amount: number,
+  prefix?: string,
+  decimalPlaces?: number
+): string => {
+  let formattedMoney = String(amount);
+  if (decimalPlaces !== undefined) {
+    formattedMoney = amount.toFixed(decimalPlaces);
+  }
+  if (prefix) {
+    formattedMoney = `${prefix}${formattedMoney}`;
+  }
+  return formattedMoney;
+};
+
+// Default parameters allow to set default values for parameters that are not provided
+const getTip = (
+  bill: number,
+  percentage: number = 20,
+  minimum: number
+): number => {
+  const calculatedTip = (percentage / 100) * bill;
+  return Math.max(calculatedTip, minimum);
+};
